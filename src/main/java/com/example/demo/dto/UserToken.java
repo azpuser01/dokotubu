aa2192cf6f554d2634b5d6e7894efa59fe8043f2
azpuser01 <intern-202010-01@azure-plus.co.jp>
2021-01-19 15:31:34 +0900

    2021/01/19_上野
    ユーザートークン

---
+++ src/main/java/com/example/demo/dto/UserToken.java
@@ -0,0 +1,44 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * ログインしているユーザーの情報を保持するトークン用DTO.
+ * 
+ * @since 2021-01-19
+ * @author ueno
+ * @author demo
+ */
+@Component
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class UserToken implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String account;
+	private int userId;
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
