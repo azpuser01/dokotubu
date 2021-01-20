91b894947b1cee8dc13312f9d71653db932f5137
Lamune <hatanakapg@gmail.com>
2021-01-20 23:50:49 +0900

    ユーザートークン取得用処理追加

--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.DokotubuConstant;
 import com.example.demo.dao.LoginDao;
+import com.example.demo.dto.UserToken;
 
 @Service
 public class LoginService implements LoginServiceInterface {
@@ -35,6 +36,22 @@ public class LoginService implements LoginServiceInterface {
 		}
 
 		return DokotubuConstant.IS_APPROVAL;
+	}
+
+	public UserToken getUserToken(String account, String password) {
+		
+		UserToken userToken = null;
+		try {
+			
+			userToken = loginDao.getUserToken(account, password);
+
+//			userId.equalsIgnoreCase("minato");
+//			userId.equalsIgnoreCase("1234");
+		} catch (NullPointerException e) {
+			return userToken;
+		}
+
+		return userToken;
 	}
 
 }
