863c0c147cfb9412a7835fcfb36029aa3262b1bd
Lamune <hatanakapg@gmail.com>
2021-01-20 21:50:25 +0900

    MessageListを取得する処理を追記

--- src/main/java/com/example/demo/controller/DokotubuController.java
+++ src/main/java/com/example/demo/controller/DokotubuController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,7 +19,11 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.DokotubuConstant;
+import com.example.demo.dao.MessageDao;
+import com.example.demo.dto.ExtendedMessage;
+import com.example.demo.dto.MessageList;
 import com.example.demo.form.LoginForm;
+import com.example.demo.form.MainForm;
 import com.example.demo.service.LoginService;
 
 @Controller
@@ -26,11 +32,13 @@ import com.example.demo.service.LoginService;
 public class DokotubuController {
 
 	private LoginService loginService;
+	private MessageDao messageDao;
 
 	@Autowired
 	DokotubuController(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
-			LoginService loginService) {
+			LoginService loginService,MessageDao messageDao) {
 		this.loginService = loginService;
+		this.messageDao = messageDao;
 	}
 
 	@ModelAttribute("loginForm")
@@ -38,42 +46,68 @@ public class DokotubuController {
 		return new LoginForm();
 	}
 
-	@GetMapping("Main") // Mainでリダイレクトされた場合動作
-	public String getMain(Model model) {
+	@GetMapping("Main") // Mainでgetされた場合動作
+	public String getMain(Model model, MessageList messageList) {
+		List<ExtendedMessage> msgList = messageDao.getAllMessage(messageList);
+		model.addAttribute("msgList", msgList);
+		
+//		for(ExtendedMessage e:msgList) {
+//			System.out.println(e.getAccount());
+//		}
+		
 		return "main";// main.htmlへ遷移
 	}
 
-	@PostMapping("Login") // Loginでpostされた場合動作 
-	public String postLogin(@ModelAttribute("loginForm") @Validated LoginForm loginForm, BindingResult result,Model model,
-			RedirectAttributes redirectAttributes) {
-		
-		if(result.hasErrors()) {//BindResultはLoginFormに値が入ってるか確認する
+	@PostMapping("Login") // Loginでpostされた場合動作
+	public String postLogin(@ModelAttribute("loginForm") @Validated LoginForm loginForm, BindingResult result,
+			Model model, RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors()) {
 			redirectAttributes.addFlashAttribute("errmsg", "アカウント名とパスワードを入力してください。");
 			return "redirect:";
 		}
 		String account = loginForm.getAccount();
 		String password = Integer.toString(loginForm.getPass());
-			DokotubuConstant loginResult = loginService.login(account, password);
+		DokotubuConstant loginResult = loginService.login(account, password);
 
-			if (loginResult.equals(DokotubuConstant.IS_APPROVAL)) {
-				return "redirect:Main";// URLを変えるためMainでリダイレクト
-			}else {
-				// 失敗時
-				redirectAttributes.addFlashAttribute("errmsg", "ログインできませんでした。");
-				return "redirect:";
-			}
+		if (loginResult.equals(DokotubuConstant.IS_APPROVAL)) {
+			return "redirect:Main";// URLを変えるためMainでリダイレクト
+		} else {
+			// 失敗時
+			redirectAttributes.addFlashAttribute("errmsg", "ログインできませんでした。");
+			return "redirect:";
+		}
+	}
+
+	@PostMapping("PostMessage") // PostMessageでpostされた場合動作
+	public String postMessage(LoginForm loginForm, @ModelAttribute("mainForm") @Validated MainForm mainForm,
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+
+		if (result.hasErrors() || mainForm.getMessage().length() == 0) {
+			redirectAttributes.addFlashAttribute("errmsg", "投稿内容がありません。");
+			return "redirect:Main";
+		}
+		String account = mainForm.getAccount();
+		String message = mainForm.getMessage();
+		DokotubuConstant postMessageResult = loginService.login(account, message);
+
+		if (postMessageResult.equals(DokotubuConstant.IS_APPROVAL)) {
+		} else {
+			// 失敗時
+			redirectAttributes.addFlashAttribute("errmsg", "投稿できませんでした。");
+		}
+		return "redirect:Main";
 	}
 
 	@GetMapping("Logout")
 	public String getLogout(@ModelAttribute("loginForm") @Validated LoginForm loginForm, SessionStatus sessionStatus) {
 		sessionStatus.setComplete();
-		return "redirect:";
+		return "index";
 	}
-	
-	@GetMapping("Registation")
-	public String getRegistation() {
-		
-		return "registation";
-		
+
+	@GetMapping("Top")
+	public String getTop(Model model) {
+		return "index";// main.htmlへ遷移
+
 	}
 }
